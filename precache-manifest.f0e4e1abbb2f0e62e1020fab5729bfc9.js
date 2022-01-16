9b4756f7e20443eb873d3f403ea44a4c0f39bc8e
Muhammad Alfaraby <mhalfaraby@gmail.com>
2022-01-16 19:48:48 +0700

    Updates

---
+++ precache-manifest.f0e4e1abbb2f0e62e1020fab5729bfc9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92b1db2034d98f38384a37e5639c0a24",
+    "url": "/alfaraby/index.html"
+  },
+  {
+    "revision": "f0147474801fbc1268d8",
+    "url": "/alfaraby/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "5caeaae02598237feb23",
+    "url": "/alfaraby/static/js/2.ea490775.chunk.js"
+  },
+  {
+    "revision": "f589bf591c94edd204e3",
+    "url": "/alfaraby/static/js/3.d439ba6f.chunk.js"
+  },
+  {
+    "revision": "f0147474801fbc1268d8",
+    "url": "/alfaraby/static/js/main.9052b70f.chunk.js"
+  },
+  {
+    "revision": "0e48b462f9622a72dc56",
+    "url": "/alfaraby/static/js/runtime~main.534d5880.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/alfaraby/static/media/logo.06e73328.svg"
+  }
+]);
